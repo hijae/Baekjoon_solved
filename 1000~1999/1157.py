4691c67fef48acbee415b231ae5ca21578129269
orangecat1199 <hijae99@gmail.com>
2020-10-18 17:14:47 +0900

    Create 1157.py

---
+++ 1000~1999/1157.py
@@ -0,0 +1,20 @@
+s=sorted(input().upper())
+alp= {}
+for i in set(s):
+    alp[i] = 0
+max=-99
+for i in s:
+    alp[i] += 1
+    if max<alp[i]:
+        max=alp[i]
+
+cnt=0
+flag=-1
+for i in alp:
+    if alp[i]==max:
+        cnt+=1
+        flag=i
+if cnt==1:
+    print(flag)
+else:
+    print("?")
