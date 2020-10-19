60264a8252935ee0b6ff81025d7c3931e359b26d
orangecat1199 <hijae99@gmail.com>
2020-10-19 17:27:12 +0900

    Create 1475.py

---
+++ 1000~1999/1475.py
@@ -0,0 +1,16 @@
+n=input()
+arr=[0,0,0,0,0,0,0,0,0,0]
+for i in n:
+    if int(i)%10==6 or int(i)%10==9:
+        arr[6]+=1
+    else:
+        arr[int(i)%10]+=1
+if arr[6]%2==0:
+    arr[6]//=2
+else:
+    arr[6]=arr[6]//2+1
+max=-99
+for i in range(0,9):
+    if arr[i]>max:
+        max=arr[i]
+print(max)
