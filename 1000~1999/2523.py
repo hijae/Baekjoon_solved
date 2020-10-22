ac463c47ac5ab4c5ac5fb6fbc5f29cda6c1579d9
Hijae song <hijae99@gmail.com>
2020-10-23 00:09:07 +0900

    Create 2523.py

---
+++ 1000~1999/2523.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(1,n):
+    for j in range(0,i):
+        print("*",end='')
+    print()
+for i in range(0,n):
+    for j in range(0,n-i):
+        print("*",end='')
+    print()
