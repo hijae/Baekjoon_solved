020f53a0704a4b269c79c6f80139d520640b71d0
Hijae song <hijae99@gmail.com>
2020-12-12 00:39:30 +0900

    Create 2942.py
    2942
    퍼거슨과 사과

---
+++ 2000~2999/2942.py
@@ -0,0 +1,18 @@
+#https://www.acmicpc.net/problem/2942
+#퍼거슨과 사과
+
+r,g=map(int,input().split())
+
+a,b=r,g
+if(a>b) : a,b = b,a
+
+while(b!=0):
+    a=a%b
+    a,b=b,a
+i=1
+while i*i<=a:
+    if a%i==0:
+        #print(i,r//i,g//i)
+        if a//i!=i:
+            print(a//i,r//(a//i),g//(a//i))
+    i+=1
