dba16bbff102853c8af40aa60af88161b0a59b70
Hijae song <hijae99@gmail.com>
2021-01-18 13:00:29 +0900

    2021.01.17

---
+++ 10000~10999/10869.py
@@ -0,0 +1,9 @@
+#https://www.acmicpc.net/problem/10869
+#사칙연산
+
+a,b=map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
---
+++ 10000~10999/10871.py
@@ -0,0 +1,9 @@
+#https://www.acmicpc.net/problem/10871
+#X보다 작은 수
+
+n,x=map(int,input().split())
+a=list(map(int,input().split()))
+for i in a:
+    if i<x:
+        print(i,end=' ')
+print()
