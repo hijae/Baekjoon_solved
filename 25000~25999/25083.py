6b37a229d4c9af8fa98a2cb124eec734995ce1ee
Hijae Song <hijae99@gmail.com>
2022-09-14 23:13:19 +0900

    25083
    새싹

---
+++ 25000~25999/25083.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/25083
+# 새싹
+
+# 파이썬에서 따옴표를 출력하기 위해선 \를 붙여야 한다.
+print("         ,r\'\"7")
+print("r`-_   ,\'  ,/")
+print(" \. \". L_r\'")
+print("   `~\/")
+print("      |")
+print("      |")
